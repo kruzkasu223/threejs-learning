b211a1bfd55e9a3a17b7fd982771e8151c121dc4
Krushang Kasundra <kruzkasu223@gmail.com>
2022-08-19 21:22:42 +0530

    done

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import "./group";
 import * as THREE from "three";
 
 // Scene
@@ -10,6 +11,32 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Position
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
+// mesh.position.normalize();
+
+// console.log(mesh.position.length());
+// console.log(mesh.position.distanceTo(camera.position));
+
+// Scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 1;
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotate
+mesh.rotation.reorder("YXZ");
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.z = Math.PI * 0.25;
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
 // Sizes
 const sizes = {
   width: 800,
@@ -19,7 +46,13 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+// camera.position.x = 1;
+// camera.position.y = 1;
 scene.add(camera);
+
+// camera.lookAt(new THREE.Vector3(3, 0, 0));
+// camera.lookAt(3, 0, 0);
+camera.lookAt(mesh.position);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
