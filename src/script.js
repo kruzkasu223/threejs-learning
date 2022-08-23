32ccb5ec5908c6fdeec812e54debf7c56e132525
Krushang Kasundra <kruzkasu223@gmail.com>
2022-08-23 19:21:31 +0530

    done

--- src/script.js
+++ src/script.js
@@ -1,57 +1,72 @@
-import "./style.css";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import "./style.css"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import * as lil from "lil-gui"
+import gsap from "gsap"
 
-const canvas = document.querySelector(".canvas");
+// Debug
+const gui = new lil.GUI({ width: 400 })
+
+const parameter = {
+  spin: () => {
+    gsap.to(mesh.rotation, { y: mesh.rotation.y + 10, duration: 1 })
+  },
+}
+gui.add(parameter, "spin")
+
+// debug panel closed by default
+gui.open(false)
+
+// hide lil-gui
+window.addEventListener("keyup", (e) => {
+  if (e.key === "h") {
+    if (gui._hidden) gui.show()
+    else gui.hide()
+  }
+})
+
+// Canvas
+const canvas = document.querySelector(".canvas")
 
 // Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-};
+}
 
 window.addEventListener("resize", () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
   // update threejs things
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-window.addEventListener("dblclick", (e) => {
-  const fullScreenElement =
-    document?.fullscreenElement || document?.webkit?.fullscreenElement;
-
-  if (!fullScreenElement) {
-    if (canvas.requestFullscreen) canvas.requestFullscreen();
-    else canvas.webkitRequestFullscreen();
-  } else {
-    if (document.exitFullscreen) document.exitFullscreen();
-    else document.webkitExitFullscreen();
-  }
-});
-
-// Cursor
-const cursor = {
-  x: 0,
-  y: 0,
-};
-window.addEventListener("mousemove", (e) => {
-  cursor.x = e.clientX / sizes.width - 0.5;
-  cursor.y = -(e.clientY / sizes.height - 0.5);
-});
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// gui.add(mesh.position, "x", -3, 3, 0.01);
+gui.add(mesh.position, "x", -3, 3, 0.01).min(-3).max(3).step(0.01)
+gui
+  .add(mesh.position, "y", -3, 3, 0.01)
+  .min(-3)
+  .max(3)
+  .step(0.01)
+  .name("elevation")
+gui.add(mesh.position, "z", -3, 3, 0.01).min(-3).max(3).step(0.01)
+
+gui.add(mesh, "visible")
+gui.add(material, "wireframe")
+gui.addColor(material, "color")
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -59,33 +74,32 @@ const camera = new THREE.PerspectiveCamera(
   sizes.width / sizes.height,
   0.1,
   100
-);
-camera.position.z = 3;
-scene.add(camera);
+)
+camera.position.z = 3
+scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-// controls.enabled = false;
-controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Animations
 
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 
 const animate = () => {
-  const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime()
 
-  controls.update();
+  controls.update()
 
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(animate);
-};
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(animate)
+}
 
-animate();
+animate()
